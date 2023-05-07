cc76730a8facfa19fc483fa3549afbc87980efa2
JoseOrdaz <joseordazsuay@gmail.com>
2023-05-07 16:19:45 +0200

    primer componente carrusel

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -3,19 +3,19 @@ import headerNavLinks from "../data/headerNavLinks";
 const LayoutWrapper = () => {
   return (
     <>
-      <header aria-label="Site Header" class="bg-white">
-        <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-          <div class="flex items-center justify-between">
-            <div class="flex-1 md:flex md:items-center md:gap-12">
-              <a class="block text-[#7843E6]" href="/">
-                <span class="sr-only">Home</span>
-                <img class="max-w-sm" src="./logo-josereact.png" />
+      <header aria-label="Site Header" className="bg-white">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between">
+            <div className="flex-1 md:flex md:items-center md:gap-12">
+              <a className="block text-[#7843E6]" href="/">
+                <span className="sr-only">Home</span>
+                <img className="max-w-sm" src="./logo-josereact.png" />
               </a>
             </div>
 
-            <div class="md:flex md:items-center md:gap-12">
-              {/* <nav aria-label="Site Nav" class="hidden md:block">
-                <ul class="flex items-center gap-6 text-sm">
+            <div className="md:flex md:items-center md:gap-12">
+              {/* <nav aria-label="Site Nav" className="hidden md:block">
+                <ul className="flex items-center gap-6 text-sm">
                   <li>
                     {headerNavLinks.map((link) => (
                       <a
@@ -30,38 +30,38 @@ const LayoutWrapper = () => {
                 </ul>
               </nav> */}
 
-              <div class="flex items-center gap-4">
-                <div class="sm:flex sm:gap-4">
-                  <a
-                    class="rounded-md bg-[#7843E6] px-5 py-2.5 text-sm font-medium text-white shadow"
-                    href="/"
+              <div className="flex items-center gap-4">
+                <div className="sm:flex sm:gap-4">
+                  <a target="_blank"
+                    className="rounded-md bg-[#7843E6] px-5 py-2.5 text-sm font-medium text-white shadow"
+                    href="https://github.com/JoseOrdaz"
                   >
                     GITHUB
                   </a>
 
-                  <div class="hidden sm:flex">
-                    <a
-                      class="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-[#7843E6]"
-                      href="/"
+                  <div className="hidden sm:flex">
+                  <a target="_blank"
+                      className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-[#7843E6]"
+                      href="https://portfolio.joseordaz.com/"
                     >
                       PORTFOLIO
                     </a>
                   </div>
                 </div>
 
-                <div class="block md:hidden">
-                  <button class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                <div className="block md:hidden">
+                  <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      class="h-5 w-5"
+                      className="h-5 w-5"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
-                      stroke-width="2"
+                      strokeWidth="2"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M4 6h16M4 12h16M4 18h16"
                       />
                     </svg>
--- src/components/carrusel/Carousel.js
+++ src/components/carrusel/Carousel.js
@@ -1,63 +1,144 @@
-import React from 'react';
-import estilos from './Slider.module.css';
+import React from "react";
+import estilos from "./Slider.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Slider({ imagenes }) {
-	// Variables y Estados
-	const [imagenActual, setImagenActual] = React.useState(0);
-	const cantidad = imagenes?.length;
+  const [imagenActual, setImagenActual] = useState(0);
+  const cantidad = imagenes?.length;
 
-	// Return prematuro para evitar errores
-	if (!Array.isArray(imagenes) || cantidad === 0) return;
+  const [autoPlay, setAutoPlay] = useState(true);
+  let timeOut = null;
 
-	const siguienteImagen = () => {
-		setImagenActual(imagenActual === cantidad - 1 ? 0 : imagenActual + 1);
-	};
+  useEffect(() => {
+    timeOut =
+      autoPlay &&
+      setTimeout(() => {
+        siguienteImagen();
+      }, 3500);
+  });
+  // console.log(imagenActual, cantidad)
 
-	const anteriorImagen = () => {
-		setImagenActual(imagenActual === 0 ? cantidad - 1 : imagenActual - 1);
-	};
+  if (!Array.isArray(imagenes) || cantidad === 0) return;
 
-	return (
+  const siguienteImagen = () => {
+    setImagenActual(imagenActual === cantidad - 1 ? 0 : imagenActual + 1);
+  };
 
-	<>
-	      <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl border-[#7843E6] border p-12 transition-all sm:w-full">
+  const anteriorImagen = () => {
+    setImagenActual(imagenActual === 0 ? cantidad - 1 : imagenActual - 1);
+  };
 
-	 <div class="mx-auto max-w-xl text-center py-12">
-      <h2 class="text-4xl font-bold sm:text-5xl">Carrusel React</h2>
+  return (
+    <>
+      <div className="relative transform overflow-hidden min-w-xl rounded-lg bg-white text-left shadow-xl border-[#7843E6] border p-12 transition-all sm:w-full">
+        <div className="mx-auto max-w-xl text-center py-12">
+          <h2 className="text-4xl font-bold sm:text-5xl">Carrusel React</h2>
 
-      <p class="mt-4 text-gray-300">
-        Un componente hecho en react para un carrusel de imagenes. 
-      </p>
-    </div>
-		<div className={estilos.container}>
-		
-			{imagenes.map((imagen, index) => {
-				return (
-					<div key={index}
-						className={
-							imagenActual === index
-								? `${estilos.slide} ${estilos.active}`
-								: estilos.slide
-						}>
-						{imagenActual === index && (
-							<img class='rounded-xl' key={index} src={imagen} alt="imagen" />
-						)}
-					</div>
-				);
-			})}
-				<button class='absolute left-8 top-50% w-11 h-11 flex justify-center items-center rounded-full shadow-md z-10 bg-gray-100 hover:bg-gray-200' onClick={anteriorImagen}><svg class=" w-8 h-8 font-bold transition duration-500 ease-in-out transform motion-reduce:transform-none text-gray-500 hover:text-gray-600 hover:-translate-x-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M15 19l-7-7 7-7">
-            </path>
-        </svg></button>
-			<button class="absolute right-8 top-50%  w-11 h-11 flex justify-center items-center rounded-full shadow-md z-10 bg-gray-100 hover:bg-gray-200" onClick={siguienteImagen}><svg class=" w-8 h-8 font-bold transition duration-500 ease-in-out transform motion-reduce:transform-none text-gray-500 hover:text-gray-600 hover:translate-x-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M9 5l7 7-7 7"></path>
-        </svg></button>
-		</div>
-		</div>
+          <p className="mt-4 text-gray-300">
+            Un componente hecho en react para un carrusel de imagenes.
+          </p>
+        </div>
+        <div
+          className={estilos.container}
+          onMouseEnter={() => {
+            setAutoPlay(false);
+            clearTimeout(timeOut);
+          }}
+          onMouseLeave={() => {
+            setAutoPlay(true);
+          }}
+        >
+          {imagenes.map((imagen, index) => {
+            return (
+              <div
+                key={index}
+                className={
+                  imagenActual === index
+                    ? `${estilos.slide} ${estilos.active}`
+                    : estilos.slide
+                }
+              >
+                {imagenActual === index && (
+                  <img
+                    className="rounded-xl"
+                    key={index}
+                    src={imagen}
+                    alt="imagen"
+                  />
+                )}
+              </div>
+            );
+          })}
+          <button
+            className="absolute left-8 top-52  w-11 h-11 flex justify-center items-center rounded-full shadow-md z-10 bg-gray-100 hover:bg-gray-200"
+            onClick={anteriorImagen}
+          >
+            <svg
+              className=" w-8 h-8 font-bold transition duration-500 ease-in-out transform motion-reduce:transform-none text-gray-500 hover:text-gray-600 hover:-translate-x-0.5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2.5"
+                d="M15 19l-7-7 7-7"
+              ></path>
+            </svg>
+          </button>
+          <button
+            className="absolute right-8 top-52 w-11 h-11 flex justify-center items-center rounded-full shadow-md z-10 bg-gray-100 hover:bg-gray-200"
+            onClick={siguienteImagen}
+          >
+            <svg
+              className=" w-8 h-8 font-bold transition duration-500 ease-in-out transform motion-reduce:transform-none text-gray-500 hover:text-gray-600 hover:translate-x-0.5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2.5"
+                d="M9 5l7 7-7 7"
+              ></path>
+            </svg>
+          </button>
 
-	
-	</>
-	);
+          <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+            <button
+            className={
+              imagenActual === 0
+                ? "w-3 h-3 rounded-full shadow-md bg-[#7843E6] hover:bg-gray-200"
+                : "w-3 h-3 rounded-full shadow-md bg-gray-100 hover:bg-gray-200"}
+              onClick={() => setImagenActual(0)}
+            ></button>
+            <button
+            className={
+              imagenActual === 1
+                ? "w-3 h-3 rounded-full shadow-md bg-[#7843E6] hover:bg-gray-200"
+                : "w-3 h-3 rounded-full shadow-md bg-gray-100 hover:bg-gray-200"}
+              onClick={() => setImagenActual(1)}
+            ></button>
+            <button
+            className={
+              imagenActual === 2
+                ? "w-3 h-3 rounded-full shadow-md bg-[#7843E6] hover:bg-gray-200"
+                : "w-3 h-3 rounded-full shadow-md bg-gray-100 hover:bg-gray-200"}
+              onClick={() => setImagenActual(2)}
+            ></button>
+
+          </div>
+
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Slider;
